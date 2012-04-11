606e372190a1487294fe66150c67ed6524c5090f
Wayne Crouch <wcrouch@wc-hp.(none)>
2012-04-11 17:22:05 +0100

    Initial Commit

--- web/app_dev.php
+++ web/app_dev.php
@@ -6,14 +6,6 @@
 
 // this check prevents access to debug front controllers that are deployed by accident to production servers.
 // feel free to remove this, extend it, or make something more sophisticated.
-//if (!in_array(@$_SERVER['REMOTE_ADDR'], array(
-//    '127.0.0.1',
-//    '::1',
-//    '192.168.1.68'
-//))) {
-//    header('HTTP/1.0 403 Forbidden');
-//    exit('You are not allowed to access this file. Check '.basename(__FILE__).' for more information.');
-//}
 
 require_once __DIR__.'/../app/bootstrap.php.cache';
 require_once __DIR__.'/../app/AppKernel.php';
