9a8090256ae1cb9c5dcd2282c7c899c72597332e
Wayne Crouch <wayne.crouch@affiliatewindow.com>
2012-05-04 15:55:02 +0100

    User entity now recognised by doctrine

--- src/itsallagile/CoreBundle/Entity/User.php
+++ src/itsallagile/CoreBundle/Entity/User.php
@@ -3,6 +3,10 @@ namespace itsallagile\CoreBundle\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
 
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="user") 
+ */
 class User
 {
     /**
@@ -31,4 +35,74 @@ class User
      * ORM\Column(type="varchar", columnDefinition="ENUM('awaitingVerification', 'Active', 'Disabled')") 
      */
     protected $status;
+
+    /**
+     * Get userId
+     *
+     * @return integer 
+     */
+    public function getUserId()
+    {
+        return $this->userId;
+    }
+
+    /**
+     * Set username
+     *
+     * @param string $username
+     */
+    public function setUsername($username)
+    {
+        $this->username = $username;
+    }
+
+    /**
+     * Get username
+     *
+     * @return string 
+     */
+    public function getUsername()
+    {
+        return $this->username;
+    }
+
+    /**
+     * Set email
+     *
+     * @param string $email
+     */
+    public function setEmail($email)
+    {
+        $this->email = $email;
+    }
+
+    /**
+     * Get email
+     *
+     * @return string 
+     */
+    public function getEmail()
+    {
+        return $this->email;
+    }
+
+    /**
+     * Set fullName
+     *
+     * @param string $fullName
+     */
+    public function setFullName($fullName)
+    {
+        $this->fullName = $fullName;
+    }
+
+    /**
+     * Get fullName
+     *
+     * @return string 
+     */
+    public function getFullName()
+    {
+        return $this->fullName;
+    }
 }
