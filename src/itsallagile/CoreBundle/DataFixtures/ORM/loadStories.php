19f3bd1c084f38509fff22a90f5999e8ca16c453
wgcrouch <wgcrouch@gmail.com>
2012-07-05 18:51:10 +0100

    Added new fixtures Fixed style of edit box

--- src/itsallagile/CoreBundle/DataFixtures/ORM/loadStories.php
+++ src/itsallagile/CoreBundle/DataFixtures/ORM/loadStories.php
@@ -9,17 +9,23 @@ use itsallagile\CoreBundle\Entity\Story;
 class LoadStories extends AbstractFixture implements OrderedFixtureInterface
 {
     public function load(ObjectManager $manager)
-    {        
-        
+    {       
         $story = new Story();
         $story->setBoard($manager->merge($this->getReference('init-board')));
         $story->setContent('Example Story');
-        $story->setOrder(0);
-        
+        $story->setSort(0);        
         $manager->persist($story);
+        
+        $story2 = new Story();
+        $story2->setBoard($manager->merge($this->getReference('init-board')));
+        $story2->setContent('Second Story');
+        $story2->setSort(1);        
+        $manager->persist($story2);
+                
         $manager->flush();
         
-        $this->addReference('init-story', $story);
+        $this->addReference('story1', $story);
+        $this->addReference('story2', $story2);
     }
 
     public function getOrder()
--- src/itsallagile/CoreBundle/DataFixtures/ORM/loadTickets.php
+++ src/itsallagile/CoreBundle/DataFixtures/ORM/loadTickets.php
@@ -12,20 +12,32 @@ class LoadTickets extends AbstractFixture implements OrderedFixtureInterface
     {        
         
         $ticket = new Ticket();
-        $ticket->setStory($manager->merge($this->getReference('init-story')));
+        $ticket->setStory($manager->merge($this->getReference('story1')));
         $ticket->setStatus($manager->merge($this->getReference('status-New')));
         $ticket->setContent('New Ticket');
-        $ticket->setType('task');
-        
+        $ticket->setType('task');        
         $manager->persist($ticket);
         
         $ticket2 = new Ticket();
-        $ticket2->setStory($manager->merge($this->getReference('init-story')));
+        $ticket2->setStory($manager->merge($this->getReference('story1')));
         $ticket2->setStatus($manager->merge($this->getReference('status-Assigned')));
         $ticket2->setContent('Assigned Ticket');
-        $ticket2->setType('defect');
-        
+        $ticket2->setType('defect');        
         $manager->persist($ticket2);
+        
+        $ticket3 = new Ticket();
+        $ticket3->setStory($manager->merge($this->getReference('story2')));
+        $ticket3->setStatus($manager->merge($this->getReference('status-Done')));
+        $ticket3->setContent('Don Ticket');
+        $ticket3->setType('bug');        
+        $manager->persist($ticket3);
+        
+        $ticket4 = new Ticket();
+        $ticket4->setStory($manager->merge($this->getReference('story2')));
+        $ticket4->setStatus($manager->merge($this->getReference('status-New')));
+        $ticket4->setContent('New Ticket in new story');
+        $ticket4->setType('bug');        
+        $manager->persist($ticket4);
         
         $manager->flush();
 
