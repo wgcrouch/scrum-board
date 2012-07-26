95e64a83ad4878841706cfcaff5af73f2b475d87
wgcrouch <wgcrouch@gmail.com>
2012-07-26 12:05:28 +0100

    Added zoomable tickets

--- src/itsallagile/ScrumboardBundle/Resources/public/js/views/ticket.js
+++ src/itsallagile/ScrumboardBundle/Resources/public/js/views/ticket.js
@@ -4,17 +4,22 @@
 itsallagile.View.Ticket = Backbone.View.extend({
     tagName: 'div',
     className: 'ticket',
-    template: '<p class="ticket-content"><%= content %></p><textarea class="ticket-input"><%= content %></textarea><i class="icon-remove delete-ticket"></i>',
+    template: '<p class="ticket-content">' +
+        '<%= content %></p><textarea class="ticket-input"><%= content %></textarea>' +
+        '<i class="icon-remove delete-ticket"></i><i class="icon-zoom-in zoom-ticket"></i>',
     events: {
         "dblclick": "startEdit",
         "blur textarea": "endEdit",
-        'hover' : 'toggleShowDelete',
-        'mouseOut' : 'toggleShowDelete',
-        'click .delete-ticket' : 'deleteConfirm'
+        'hover' : 'toggleShowIcons',
+        'mouseOut' : 'toggleShowIcons',
+        'click .delete-ticket' : 'deleteConfirm',
+        'click .zoom-ticket' : 'zoomToggle'
     },
     storyView: null,
     
-    //Initialize bindings to changes in the model
+    /**
+     * Initialize bindings to changes in the model
+     */
     initialize: function(options) {
         this.model.bind('change', this.refresh);
         this.model.bind('sync', this.refresh);
@@ -22,7 +27,9 @@ itsallagile.View.Ticket = Backbone.View.extend({
         _.bindAll(this);
     },
     
-    //Render a ticket
+    /**
+     * Render a ticket
+     */
     render: function() {
         this.id = this.model.get('id');
         this.$el.addClass(this.model.get('type'));
@@ -34,13 +41,17 @@ itsallagile.View.Ticket = Backbone.View.extend({
         return this;
     },
     
-    //Show the edit box when ticket is double clicked
+    /**
+     * Show the edit box when ticket is double clicked
+     */
     startEdit: function() {
         $('p',this.$el).hide();
         $('textarea', this.$el).show().focus();
     },
     
-    //Save the ticket when editing has finished
+    /**
+     * Save the ticket when editing has finished
+     */
     endEdit: function() {
         var p = $('p', this.$el);
         var text = $('textarea', this.$el);
@@ -52,17 +63,41 @@ itsallagile.View.Ticket = Backbone.View.extend({
        
     },
     
-    //Callback function for successfully changing a tickets contents
+    /**
+     * Callback function for successfully changing a tickets contents
+     */
     changeSuccess: function(model, response) {
         if (typeof itsallagile.socket !== 'undefined') {
             itsallagile.socket.emit('ticket:change', itsallagile.roomId, response);
         }
     },
     
-    toggleShowDelete: function() {
+    /**
+     * Show the delete icon when hovering over a ticket
+     */
+    toggleShowIcons: function() {
         $('.delete-ticket', this.$el).fadeToggle('fast');
+        $('.zoom-ticket', this.$el).fadeToggle('fast');
     },
     
+    /**
+     * Zoom in/out of a ticket
+     */
+    zoomToggle: function() {        
+        if (this.$el.hasClass('zoomed')) {
+            this.$el.removeClass('zoomed');
+            this.$el.animate({width:90, height:90});
+            $('.zoom-ticket', this.$el).removeClass('icon-zoom-out').addClass('icon-zoom-in');
+        } else {
+            this.$el.addClass('zoomed');
+            this.$el.animate({width:190, height:190});
+            $('.zoom-ticket', this.$el).removeClass('icon-zoom-in').addClass('icon-zoom-out');
+        }
+    },
+    
+    /**
+     * Show a dialog to confirm deleting a ticket
+     */
     deleteConfirm: function(event) {
         if (confirm('Are you sure you want to delete this ticket?')) {
             this.model.destroy({silent: true});
