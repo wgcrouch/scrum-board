7038b007fe504a28f80879e64f8de1a810dd9256
wgcrouch <wgcrouch@gmail.com>
2012-11-08 01:42:23 +0000

    Code standards

--- src/itsallagile/ScrumboardBundle/Controller/ScrumboardController.php
+++ src/itsallagile/ScrumboardBundle/Controller/ScrumboardController.php
@@ -4,13 +4,10 @@ namespace itsallagile\ScrumboardBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
-
 class ScrumboardController extends Controller
 {
-    
     public function indexAction($slug)
     {
-             
         $em = $this->getDoctrine()->getEntityManager();
         $query = $em->createQuery(
             'SELECT b FROM itsallagileCoreBundle:Board b WHERE b.boardId = :slug OR b.slug = :slug'
@@ -20,15 +17,15 @@ class ScrumboardController extends Controller
 
         if (!$board) {
             throw $this->createNotFoundException('No board found for slug ' . $slug);
-        }       
-        
+        }
+
         $repository = $this->getDoctrine()->getRepository('itsallagileCoreBundle:Status');
         $statusArray = array();
         $statuses = $repository->findAll();
-        foreach($statuses as $status) {
+        foreach ($statuses as $status) {
             $statusArray[] = $status->getArray();
         }
-        
+
         $viewData = array('board' => $board->getArray(), 'statuses' => $statusArray);
 
         return $this->render('itsallagileScrumboardBundle:Board:index.html.twig', $viewData);
