0befe3dce9f33a7fd30dad0b3f8b6bebd85e81ba
wgcrouch <wgcrouch@gmail.com>
2012-11-16 13:59:46 +0000

    Added initial tests for the Api

---
+++ src/itsallagile/APIBundle/Tests/Controller/BoardsControllerTest.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace itsallagile\ApiBundle\Tests\Controller;
+
+use itsallagile\APIBundle\Tests\ApiTestCase;
+
+class BoardsControllerTest extends ApiTestCase
+{
+    
+    public function testGetAll()
+    {
+        $client = static::createClient();
+
+        $client->request('GET', '/api/boards', array(), array(), $this->userAuth);
+        
+        $response = $client->getResponse();
+        $this->assertJsonResponse($response);
+        $result = json_decode($response->getContent(), true);
+        
+        $this->assertEquals(2, count($result));
+        $this->assertEquals('Example Board', $result[1]['name']);
+    }
+    
+    public function testGetOneWithValidId()
+    {
+        $client = static::createClient();
+
+        $client->request('GET', '/api/boards/1', array(), array(), $this->userAuth);
+        
+        $response = $client->getResponse();
+        $this->assertJsonResponse($response);
+        $result = json_decode($response->getContent(), true);
+
+        $this->assertEquals('Example Board', $result['name']);
+        
+        $params = array('id', 'name', 'slug', 'team', 'stories');
+        foreach ($params as $param) {
+            $this->assertTrue(array_key_exists($param, $result));
+        }
+        
+    }
+    
+    public function testGetOneWithInValidId()
+    {
+        $client = static::createClient();
+
+        $client->request('GET', '/api/boards/fooo', array(), array(), $this->userAuth);
+        
+        $response = $client->getResponse();
+        $this->assertJsonResponse($response, 404);
+                
+    }        
+}
